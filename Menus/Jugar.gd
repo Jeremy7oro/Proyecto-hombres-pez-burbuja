b81658945a43a2a5d18444befabca59e5eb2223d
Victor Martin Ortiz Palacio <martinortizpalacio@gmail.com>
2025-01-25 17:26:23 -0300

    Add audio manager and audio control

--- Menus/Jugar.gd
+++ Menus/Jugar.gd
@@ -9,3 +9,8 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+
+func _on_pressed() -> void:
+	AudioManager.coin_effect.play()
---
+++ Scripts/AudioManager.gd
@@ -0,0 +1,3 @@
+extends Node
+@onready var bg_music: AudioStreamPlayer = $bg_music
+@onready var coin_effect: AudioStreamPlayer = $coin_effect
