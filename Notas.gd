98a0f4a459394abe513b2b5bb40acfccea412451
Jeremy7oro <jeremytoro220@gmail.com>
2025-01-25 03:32:39 -0300

    Cree unas "Notas.gd" para comunicarnos dentro de Godot

---
+++ Notas.gd
@@ -0,0 +1,5 @@
+extends Label
+
+
+# Para comunicar se debe utilizar "#", asi no generará ningun tipo de herror
+# Recomiendo precionar "Editar" (que se encuentra en la parte superior de este .gd) y luego precionar "Toggle Word Warp" (esto hara mas facil la lectura)
