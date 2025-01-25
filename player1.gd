b8eb92540e5ecc038251825a9e10ed78ee1b6deb
Jeremy7oro <jeremytoro220@gmail.com>
2025-01-25 12:37:28 -0300

    Prototipo 1

---
+++ player1.gd
@@ -0,0 +1,35 @@
+extends Node2D
+
+var Escudo : int 
+var Espada : int
+
+
+func _ready() -> void:
+	Escudo = 0
+	Espada = 0
+
+
+func _process(delta: float) -> void:
+	$Label.text = str(Escudo)
+	$Label2.text = str(Espada)
+	if Escudo <= 0:
+		if Input.is_action_just_released("Arriba"):
+			Escudo = 1
+		if Input.is_action_just_released("Medio"):
+			Escudo = 2
+		if Input.is_action_just_released("Abajo"):
+			Escudo = 3
+	elif Espada <= 0:
+		if Input.is_action_just_released("Arriba"):
+			Espada = 1
+		if Input.is_action_just_released("Medio"):
+			Espada = 2
+		if Input.is_action_just_released("Abajo"):
+			Espada = 3
+	if Espada == Escudo:
+		Espada = 0
+	
+	#Control
+	if $"../tiempo".value == $"../tiempo".max_value:
+		GLOBAL.Escudo_P1 = Escudo
+		GLOBAL.Espada_P1 = Espada
