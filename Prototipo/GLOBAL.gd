2435493f5978cf14d86b67daae33a418843723f2
Jeremy7oro <jeremytoro220@gmail.com>
2025-01-26 05:42:53 -0300

    Pruebas visuales

--- Prototipo/GLOBAL.gd
+++ Prototipo/GLOBAL.gd
@@ -8,26 +8,28 @@ var Escudo_P2 : int
 
 var Turno : bool = false
 var Win : bool
+var Lose : bool
 
 
 func _ready() -> void:
 	Win = false
+	Lose = false
 
 
 func _process(delta: float) -> void:
 	if Turno == true:
 		if Espada_P1 > 0 and Espada_P2 > 0:
-			if Espada_P1 == Espada_P2:
-				pass
-			if Espada_P1 == Escudo_P2:
-				pass
 			if not Espada_P1 == Espada_P2 and not Espada_P1 == Escudo_P2:
 				Win = true
+			
+			if not Espada_P2 == Espada_P1 and not Espada_P2 == Escudo_P1:
+				Lose = true
 	
 	#RESET
 	if Input.is_key_pressed(KEY_R):
 		get_tree().change_scene_to_file("res://Prototipo/PvCPU.tscn")
 		Win = false
+		Lose = false
 		Espada_P1 = 0
 		Escudo_P1 = 0
 		Espada_P2 = 0
--- Prototipo/cpu.gd
+++ Prototipo/cpu.gd
@@ -3,12 +3,12 @@ extends Node2D
 var Escudo : int
 var Espada : int
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+#Animaciones
+@onready var esc_pos = $Escudo2.position
+@onready var esp_pos = $Espada2.position
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _process(delta: float) -> void:
 	$Label.text = str(GLOBAL.Escudo_P2)
 	$Label2.text = str(GLOBAL.Espada_P2)
@@ -16,7 +16,15 @@ func _process(delta: float) -> void:
 		if Escudo <= 0:
 			Escudo = randi_range(1,3)
 		else:
-				while Espada <= 0 or Espada == Escudo:
-					Espada = randi_range(1,3)
+				while Espada <= 0:
+					var rang = randi_range(1,3)
+					if not rang == Escudo:
+						Espada = rang
+						$Escudo2.position = esc_pos + Vector2(-200,150*(Escudo-2))
+						$Espada2.position = esp_pos + Vector2(-220,150*(Espada-2))
+						$Espada2.rotation = 90
 		GLOBAL.Escudo_P2 = Escudo
 		GLOBAL.Espada_P2 = Espada
+		
+		#Aimación
+		
