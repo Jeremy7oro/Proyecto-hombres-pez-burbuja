165668550b3880eb96a107c144192d03ceeed335
Jeremy7oro <jeremytoro220@gmail.com>
2025-01-26 06:36:20 -0300

    Detalles

--- Prototipo/player1.gd
+++ Prototipo/player1.gd
@@ -17,21 +17,21 @@ func _process(delta: float) -> void:
 	$Label2.text = str(GLOBAL.Espada_P1)
 	if $"../tiempo".value < $"../tiempo".max_value:
 		if Escudo <= 0:
-			if Input.is_action_just_pressed("Arriba"):
+			if Input.is_action_pressed("Arriba"):
 				Escudo = 1
 				$Escudo.position = esc_pos + Vector2(200, -150)
-			if Input.is_action_just_pressed("Medio"):
+			if Input.is_action_pressed("Medio"):
 				Escudo = 2
 				$Escudo.position = esc_pos + Vector2(200, 0)
-			if Input.is_action_just_pressed("Abajo"):
+			if Input.is_action_pressed("Abajo"):
 				Escudo = 3
 				$Escudo.position = esc_pos + Vector2(200,150)
 		elif Espada <= 0:
-			if Input.is_action_just_pressed("Arriba") and not Escudo == 1:
+			if Input.is_action_pressed("Arriba") and not Escudo == 1:
 				Espada = 1
-			if Input.is_action_just_pressed("Medio") and not Escudo == 2:
+			if Input.is_action_pressed("Medio") and not Escudo == 2:
 				Espada = 2
-			if Input.is_action_just_pressed("Abajo") and not Escudo == 3:
+			if Input.is_action_pressed("Abajo") and not Escudo == 3:
 				Espada = 3
 		elif  Espada > 0:
 			$Espada.position = esp_pos + Vector2(220,150*(Espada-2))
